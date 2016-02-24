d756163eece628dd437117ba9c8ed3c13f79e7ff
OmondiKevin <kevochi09@gmail.com>
2016-02-24 15:30:22 +0300

    new templates

--- application/modules/template/controllers/template.php
+++ application/modules/template/controllers/template.php
@@ -1,15 +1,18 @@
 <?php
-if(!defined('BASEPATH')) exit('No direct script access allowed!');
-/**
-* 
-*/
-class Template extends MY_Controller
-{
-	
-	function __construct()
-	{
-		parent:: __construct();
-	}
-}
+if (!defined('BASEPATH'))
+	exit('No direct script access allowed');
 
-?>
+class Template extends MY_Controller {
+
+	// a function that calls the template view
+
+	protected function load_view($data){
+		$this->load->view('template_view',$data);
+
+	}
+	protected function admin_view($info){
+		$this->load->view('admin_template_view',$info);
+
+	}
+
+}
