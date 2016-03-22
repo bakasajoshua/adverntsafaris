7b76761e90962aa48d4c5ef064c7cd668963b925
Joshua Bakasa <baksajoshua09@gmail.com>
2016-03-22 16:55:19 +0300

    some few tweeks to the signup view

--- application/modules/login/views/login_view2.php
+++ application/modules/login/views/login_view2.php
@@ -25,20 +25,31 @@
 );
 
 ?>
-<div class="row">
-  <?php echo form_open(base_url().'login/authentication', $form_attributes);?>
+<style type="text/css">
+    .panel {
+        width: 380px;
+        /*position: absolute;*/
+        margin: auto;
+    }
+</style>
+<div class="panel panel-default">
+  <div class="panel-body">
     <div class="row">
-      <div class="input-field col s12">
-        <?php echo form_input($email_field); ?>
-        <label for="email_address">Email</label>
-      </div>
+      <?php echo form_open(base_url().'login/authentication', $form_attributes);?>
+        <div class="row">
+          <div class="input-field col s12">
+            <?php echo form_input($email_field); ?>
+            <label for="email_address">Email</label>
+          </div>
+        </div>
+        <div class="row">
+          <div class="input-field col s12">
+            <?php echo form_password($password_field); ?>
+            <label for="password">Password</label>
+          </div>
+        </div>
+        <?php echo form_button($button);?>
+      <?php echo form_close();?>
     </div>
-    <div class="row">
-      <div class="input-field col s12">
-        <?php echo form_password($password_field); ?>
-        <label for="password">Password</label>
-      </div>
-    </div>
-    <?php echo form_button($button);?>
-  <?php echo form_close();?>
+  </div>
 </div>
